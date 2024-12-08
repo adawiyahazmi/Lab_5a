afd8caab26f82516ac7a372232c1ec0eb12c033d
Rabiatul Adawiyah Binti Azmi <rabiatulateh03@gmail.com>
2024-12-08 09:33:43 +0800

    initial commit with lab5a_q1.php

---
+++ Lab5a_q1.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Lab 5a Q1</title>
+    <style>
+        table {
+            width: 50%;
+            border-collapse: collapse;
+            margin: 20px auto;
+        }
+        table, th, td {
+            border: 1px solid black;
+        }
+        th, td {
+            padding: 10px;
+            text-align: left;
+        }
+        th {
+            background-color: #f2f2f2;
+        }
+    </style>
+</head>
+<body>
+<?php
+$name = "Rabiatul Adawiyah";
+$matric_number = "AI220098";
+$course = "Software Engineering";
+$year_of_study = "Year 3";
+$address = "12, Jalan Universiti, Batu Pahat.";
+?>
+
+<table>
+    <tr>
+        <th>Field</th>
+        <th>Details</th>
+    </tr>
+    <tr>
+        <td>Name</td>
+        <td><?php echo $name; ?></td>
+    </tr>
+    <tr>
+        <td>Matric Number</td>
+        <td><?php echo $matric_number; ?></td>
+    </tr>
+    <tr>
+        <td>Course</td>
+        <td><?php echo $course; ?></td>
+    </tr>
+    <tr>
+        <td>Year of Study</td>
+        <td><?php echo $year_of_study; ?></td>
+    </tr>
+    <tr>
+        <td>Address</td>
+        <td><?php echo $address; ?></td>
+    </tr>
+</table>
+
+</body>
+</html>
